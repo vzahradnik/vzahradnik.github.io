3b4d7fd0951310e43668af3c3015084ced27b971
Vladimir Zahradnik <vladimir@zahradnik.io>
2020-01-05 17:54:11 +0100

    Fill custom social links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,29 @@
 module.exports = {
   siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `Vladimír Záhradník | Blog`,
+    name: `Vladimír Záhradník`,
+    siteUrl: `https://zahradnik.io`,
+    description: `A personal blog about entrepreneurship, software development, open-source, and content creation.`,
     hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+      heading: `Welcome! If you're interested in entrepreneurship and software development, you're in the right place.`,
       maxWidth: 652,
     },
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/narative`,
+        url: `https://twitter.com/VladoZahradnik`,
       },
       {
         name: `github`,
-        url: `https://github.com/narative`,
+        url: `https://github.com/vzahradnik`,
       },
       {
         name: `instagram`,
-        url: `https://instagram.com/narative.co`,
+        url: `https://www.instagram.com/vladimir.zahradnik`,
       },
       {
         name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
+        url: `https://www.linkedin.com/in/vladimirzahradnik`,
       },
     ],
   },
